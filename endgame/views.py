bbb7a28111ba4d51d70200836ab3352a3a644273
alex-chew <mathmad2008@yahoo.com>
2014-07-15 16:02:21 -0700

    Move blueprint to view

--- endgame/views.py
+++ endgame/views.py
@@ -1,11 +1,22 @@
 from endgame import app
 from flask import render_template, request
 
-from endgame.bp_home import bp_home
-
-app.register_blueprint(bp_home)
-
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/login')
+def _login():
+	return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def _register():
+	message = None
+	if request.method == 'POST':
+		u = request.form['username']
+		p = request.form['password']
+		return redirect(url_for('_login'))
+	else:
+		message = 'dickbutt error'
+	return render_template('register.html', message=message)
 
