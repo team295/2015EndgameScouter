652c565cf20012607c84a4ecb77f02f369a15f4c
David Song <davidsongis@gmail.com>
2014-07-15 15:17:18 -0700

    deleted trash python files

--- endgame/main.py
+++
@@ -1,9 +0,0 @@
-import os
-from flask import Flask, redirect, url_for, render_template
-
-app = Flask(__name__)
-app.config.from_object(__name__)
-
-@app.route('/')
-def showHome():
-    return render_template('home.html')
