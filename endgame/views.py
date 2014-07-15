8cf9bb9f601ab40356d2a37cfefbead058935c68
alex-chew <mathmad2008@yahoo.com>
2014-07-15 15:11:01 -0700

    Add register page

--- endgame/views.py
+++ endgame/views.py
@@ -1,6 +1,16 @@
 from endgame import app
-from flask import render_template
+from flask import render_template, request
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+	if request.method == 'POST':
+		u = request.form['username']
+		p = request.form['password']
+	else:
+		message = 'Something happened.'
+	return render_template('register.html', message=message)
+
