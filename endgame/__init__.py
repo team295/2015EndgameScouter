70a823061e63906bf1ac9f770eb74180c2bb92dc
David Song <davidsongis@gmail.com>
2014-07-16 17:02:22 -0700

    Started working on user registration

--- endgame/__init__.py
+++ endgame/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
-
+from flask.ext.sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.config.from_object('endgame.config')
+db = SQLAlchemy(app)
 
-import endgame.views
+import endgame.views, endgame.dbmodel
