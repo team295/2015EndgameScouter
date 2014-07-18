be779813f9db4d718107f89501dcd328e961380c
alex-chew <mathmad2008@yahoo.com>
2014-07-18 13:45:58 -0700

    Changed CD to Endgame

--- endgame/dbmodel.py
+++ endgame/dbmodel.py
@@ -2,16 +2,17 @@ from endgame import db
 from flask.ext.sqlalchemy import SQLAlchemy
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(40), unique = True)
-    admin = db.Column(db.Boolean, default = False)
-    approved = db.Column(db.Boolean,default = False)
-    username = db.Column(db.String(80), unique = True)
-    password = db.Column(db.String(80), unique = True)
+	id = db.Column(db.Integer, primary_key = True)
+	name = db.Column(db.String(40), unique = True)
+	admin = db.Column(db.Boolean, default = False)
+	approved = db.Column(db.Boolean,default = False)
+	username = db.Column(db.String(80), unique = True)
+	password = db.Column(db.String(80), unique = True)
 
-    def __init__(self,name,username, password):
-        self.username = username
-        self.password = password
-        self.name = name
-    def __repr__(self):
-        return '<USER %r>' % self.username
+	def __init__(self,name,username, password):
+		self.username = username
+		self.password = password
+		self.name = name
+	
+	def __repr__(self):
+		return '<USER %r>' % self.username
