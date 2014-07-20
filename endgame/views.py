bbebe812622030544422c69a93d99ca08ec3354e
alex-chew <mathmad2008@yahoo.com>
2014-07-19 22:57:35 -0700

    Add login/logout functionality

--- endgame/views.py
+++ endgame/views.py
@@ -1,5 +1,5 @@
 from endgame import app
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, session, flash
 from flask.ext.sqlalchemy import SQLAlchemy
 from endgame.dbmodel import *
 from sqlalchemy import exc
@@ -8,23 +8,43 @@ from sqlalchemy import exc
 def _home():
 	return render_template('home.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def _login():
+	if session.get('logged_in'):
+		flash('You are already logged in as {}.'.format(session.get('username')))
+		return redirect(url_for('_home'))
+	if request.method == 'POST':
+		try:
+			u = request.form['username']
+			p = request.form['password']
+			if User.query.filter(User.username == u, User.password == p).first() != None:
+				session['logged_in'] = True
+				session['username'] = u
+				flash('Logged in as {}.'.format(u))
+				return redirect(url_for('_home'))
+			else:
+				flash('Incorrect username or password.')
+		except exc.SQlAlchemyError:
+			flash('There was a problem accessing the database.')
 	return render_template('login.html')
+
+@app.route('/logout')
+def _logout():
+	session.pop('logged_in', None)
+	flash('Logged out.')
+	return redirect(url_for('_home'))
 
 @app.route('/admin')
 def _admin():
-	users = [] 
-	message = None
+	users = []
 	try:
 		users = list(User.query.all())
 	except exc.SQLAlchemyError:
-		message = 'There was a problem reading the users from the database.'
-	return render_template('admin.html', users=users, message=message)
+		flash('There was a problem reading the users from the database.')
+	return render_template('admin.html', users=users)
 
 @app.route('/register', methods=['GET', 'POST'])
 def _register():
-	message = None
 	if request.method == 'POST':
 		try:
 			n = request.form['name']
@@ -32,8 +52,9 @@ def _register():
 			p = request.form['password']
 			db.session.add(User(n, u, p))
 			db.session.commit()
+			flash('You are now registered as {}.'.format(u))
 			return redirect(url_for('_home'))
 		except exc.SQLAlchemyError:
-			message = 'There was a problem adding a user to the database.'
-	return render_template('register.html', message=message)
+			flash('There was a problem adding a user to the database.')
+	return render_template('register.html')
 
